3f85b25e4452f5902c6bc5c33bc0fdf15acf1043
liyanxin001 <liyanxin003@126.com>
2026-05-22 21:02:28 +0800

    1

--- Experiment/src/com/greatfree/cluster/experiment/client/StudentMenuOptions.java
+++ Experiment/src/com/greatfree/cluster/experiment/client/StudentMenuOptions.java
@@ -3,7 +3,10 @@ package com.greatfree.cluster.experiment.client;
 public final class StudentMenuOptions {
 	
 	public final static int NO_OPTION = -1;
-	public final static int UPLOAD_PROCEDURE_REQEUST = 1;
+	public final static int UPLOAD_PROCEDURE = 1;
+	public final static int UPLOAD_IMAGE = 2;
+	public final static int VIEW_MY_PROCEDURES = 3;
+	public final static int VIEW_COMMENT = 4;
 	
 
 }
