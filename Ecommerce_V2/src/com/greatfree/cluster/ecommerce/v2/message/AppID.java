6a98e275f0f5badd1bf02cd8f3b2fe52a759bf1e
liyanxin001 <liyanxin003@126.com>
2026-06-25 23:58:19 +0800

    1

--- Ecommerce_V2/src/com/greatfree/cluster/ecommerce/v2/message/AppID.java
+++ Ecommerce_V2/src/com/greatfree/cluster/ecommerce/v2/message/AppID.java
@@ -6,6 +6,7 @@ public final class AppID {
 	public static final int CREATE_STORE_RESPONSE = 80001;
 	public static final int SEARCH_FOR_PRODUCTS_REQUEST = 80002;
 	public static final int SEARCH_FOR_PRODUCTS_RESPONSE = 80003;
+	
 	public static final int ADD_TO_CART_REQUEST = 80004;
 	public static final int ADD_TO_CART_RESPONSE = 80005;
 	public static final int CART_REGISTRY_REQUEST = 80006;
@@ -16,6 +17,11 @@ public final class AppID {
 	public static final int GET_CART_RESPONSE = 80011;
 	public static final int PUT_ON_SALE_NOTIFICATION = 80012;
 	public static final int REMOVE_FROM_SALE_NOTIFICATION = 80013;
+	public static final int UPDATE_QUANTITY_REQUEST = 80014;
+	public static final int UPDATE_QUANTITY_RESPONSE = 80015;
+	
+	public static final int WITHDRAW_FROM_STORE_REQUEST = 80016;
+	public static final int WITHDRAW_FROM_STORE_RESPONSE = 80017;
 
 
 }
