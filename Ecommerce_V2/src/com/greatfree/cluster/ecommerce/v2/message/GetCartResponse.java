2b120456568f8007283ca48be9c32d3b91ce7fd7
liyanxin001 <liyanxin003@126.com>
2026-07-03 23:58:19 +0800

    1

--- Ecommerce_V2/src/com/greatfree/cluster/ecommerce/v2/message/GetCartResponse.java
+++ Ecommerce_V2/src/com/greatfree/cluster/ecommerce/v2/message/GetCartResponse.java
@@ -1,5 +1,12 @@
 package com.greatfree.cluster.ecommerce.v2.message;
 
-public class GetCartResponse {
+import edu.greatfree.multicast.message.MulticastResponse;
+
+public class GetCartResponse extends MulticastResponse{
+
+	public GetCartResponse(int type, String collaboratorKey) {
+		super(type, collaboratorKey);
+		// TODO Auto-generated constructor stub
+	}
 
 }
