9f1c7f075e82a7120ab6f8279a47e981a5a32a8d
liyanxin001 <liyanxin003@126.com>
2026-07-28 22:05:48 +0800

    1

---
+++ Ecommerce/src/com/greatfree/cluster/ecommerce/v1/message/GetCartRequest.java
@@ -0,0 +1,37 @@
+package com.greatfree.cluster.ecommerce.v1.message;
+
+import edu.greatfree.cluster.message.ClusterRequest;
+
+
+public class GetCartRequest extends ClusterRequest {
+	
+	
+
+	private static final long serialVersionUID = -8798567153935681724L;
+	
+	
+	private String userName;
+	
+	
+
+	public GetCartRequest(String userName) {
+		super(userName,AppID.GET_CART_REQUEST);
+		this.userName = userName;
+	}
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+	public void setUserName(String clientID) {
+		this.userName = clientID;
+	}
+
+	
+	
+
+}
